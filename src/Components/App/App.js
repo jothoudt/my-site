27973b382c799b04fd494704ee7f74c2ec3e217a
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-08-18 21:13:09 -0500

    install bootstrap and add bootstrap header

---
+++ src/Components/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//components
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+import Home from '../Home/Home'
+//css import
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Home />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
