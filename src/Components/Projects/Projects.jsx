164020791b1c345ccfcca05bec15dae4a27ac468
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-09-07 21:39:49 -0500

    mealreel component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import YourPath from '../YourPath/YourPath';
+import MealReel from '../MealReel/MealReel';
 
 function Projects(){
     return(
         <div>
             <YourPath />
+            <MealReel />
         </div>
     )
 }
