294d42e7345c84217b563438c9ab42fd4e217d67
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-09-09 16:22:33 -0500

    change projects design

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,13 +1,89 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import YourPath from '../YourPath/YourPath';
 import MealReel from '../MealReel/MealReel';
 
 function Projects(){
+    function TabPanel(props) {
+        const { children, value, index, ...other } = props;
+      
+        return (
+          <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+          >
+            {value === index && (
+              <Box p={3}>
+                <Typography>{children}</Typography>
+              </Box>
+            )}
+          </div>
+        );
+      }
+      
+      TabPanel.propTypes = {
+        children: PropTypes.node,
+        index: PropTypes.any.isRequired,
+        value: PropTypes.any.isRequired,
+      };
+      
+      function a11yProps(index) {
+        return {
+          id: `full-width-tab-${index}`,
+          'aria-controls': `full-width-tabpanel-${index}`,
+        };
+      }
+      
+      const useStyles = makeStyles((theme) => ({
+        root: {
+          backgroundColor: theme.palette.background.paper,
+          width: '100vw',
+        },
+      }));
+      
+    //   export default function FullWidthTabs() {
+        const classes = useStyles();
+        const theme = useTheme();
+        const [value, setValue] = React.useState(0);
+      
+        const handleChange = (event, newValue) => {
+          setValue(newValue);
+        };
+      
+        const handleChangeIndex = (index) => {
+          setValue(index);
+        };
     return(
-        <div>
+        <div className={classes.root}>
+        {/* <AppBar position="static" color="default"> */}
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+            style={{backgroundColor:"#E5E4E2", color:"#000000"}}
+
+          >
+            <Tab label="YourPath" {...a11yProps(0)} />
+            <Tab label="MealReel" {...a11yProps(1)} />
+          </Tabs>
+          <TabPanel value={value} index={0} dir={theme.direction}>
             <YourPath />
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
             <MealReel />
-        </div>
+          </TabPanel>
+      </div>
     )
 }
 
